63d17270d145baf8caac612c14ce19df23bfb534
Tashlyne Daniels <106475757+Tdaniels25@users.noreply.github.com>
2023-06-20 11:05:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,67 +15,100 @@ let activePlayer = 0;
 
 // Function to change the player name
 function changeNames() {
-    const player1Name = prompt("Enter Player 1 Name");
-    const player2Name = prompt("Enter Player 2 Name");
+  const player1Name = prompt("Enter Player 1 Name");
+  const player2Name = prompt("Enter Player 2 Name");
 
-    document.querySelector(".player1").textContent = player1Name;
-    document.querySelector(".player2").textContent = player2Name;
+  document.querySelector(".player1").textContent = player1Name;
+  document.querySelector(".player2").textContent = player2Name;
 }
 
 // Function to roll the dice
 function rollTheDice() {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
 
-    document.querySelector(".img1").setAttribute("src", "dice" + randomNumber + ".png");
+  document.querySelector(".img1").setAttribute("src", "dice" + randomNumber + ".png");
 
-    if (randomNumber === 1) {
-        currentScore = 0;
-        document.getElementById(`current_${activePlayer}`).textContent = currentScore;
-        switchPlayer();
-    } else {
-        currentScore += randomNumber;
-        document.getElementById(`current_${activePlayer}`).textContent = currentScore;
-    }
+  if (randomNumber === 1) {
+    currentScore = 0;
+    document.getElementById(`current_${activePlayer}`).textContent = currentScore;
+    switchPlayer();
+  } else {
+    currentScore += randomNumber;
+    document.getElementById(`current_${activePlayer}`).textContent = currentScore;
+  }
 }
 
 // Function to switch players
 function switchPlayer() {
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    document.querySelector(".player1").classList.toggle("player_active");
-    document.querySelector(".player2").classList.toggle("player_active");
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document.querySelector(".player1").classList.toggle("player_active");
+  document.querySelector(".player2").classList.toggle("player_active");
 }
 
 // Function to hold the current score
 function hold() {
-    scores[activePlayer] += currentScore;
-    document.getElementById(`score_${activePlayer}`).textContent = scores[activePlayer];
-    currentScore = 0;
-    document.getElementById(`current_${activePlayer}`).textContent = currentScore;
-    switchPlayer();
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score_${activePlayer}`).textContent = scores[activePlayer];
+  currentScore = 0;
+  document.getElementById(`current_${activePlayer}`).textContent = currentScore;
+  switchPlayer();
+  checkWinner();
+}
+
+// Function to check the winner
+function checkWinner() {
+  if (scores[activePlayer] >= 30) {
+    document.querySelector("h1").textContent = `Player ${activePlayer + 1} Wins!`;
+    disableButtons();
+  }
+}
+
+// Function to disable buttons when the game ends
+function disableButtons() {
+  document.querySelector(".butn_roll").disabled = true;
+  document.querySelector(".btn_hold").disabled = true;
 }
 
 // Function for a new game
 function newGame() {
-    scores = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
 
-    score0.textContent = 0;
-    score1.textContent = 0;
-    current0.textContent = 0;
-    current1.textContent = 0;
+  score0.textContent = 0;
+  score1.textContent = 0;
+  current0.textContent = 0;
+  current1.textContent = 0;
 
-    document.querySelector("h1").innerHTML = "Let's Play";
+  document.querySelector("h1").innerHTML = "Let's Play";
 
-    document.querySelector(".player1").classList.add("player_active");
-    document.querySelector(".player2").classList.remove("player_active");
+  document.querySelector(".player1").classList.add("player_active");
+  document.querySelector(".player2").classList.remove("player_active");
+
+  enableButtons();
+}
+
+// Function to enable buttons for a new game
+function enableButtons() {
+  document.querySelector(".butn_roll").disabled = false;
+  document.querySelector(".btn_hold").disabled = false;
 }
 
 // Event listener for the "Roll Dice" button
-document.querySelector(".butn_roll").addEventListener("click", rollTheDice);
+document.querySelector(".butn_roll").addEventListener("click", function() {
+  // Check if the game is still in progress
+  if (scores[0] < 30 && scores[1] < 30) {
+    rollTheDice();
+  }
+});
 
 // Event listener for the "Hold" button
-document.querySelector(".btn_hold").addEventListener("click", hold);
+document.querySelector(".btn_hold").addEventListener("click", function() {
+  // Check if the game is still in progress
+  if (scores[0] < 30 && scores[1] < 30) {
+    hold();
+  }
+});
 
 // Event listener for the "New Game" button
 document.querySelector(".butn_newgame").addEventListener("click", newGame);
